c0249a04f238e435d5340868c6c0ce31c1878f4f
orbitlens <orbit.lens@gmail.com>
2020-07-18 01:27:58 +0300

    Improve random token generation logic in the intent lib

--- intent/src/random-token-generator.js
+++ intent/src/random-token-generator.js
@@ -1,7 +1,11 @@
 export function generateRandomToken() {
-    const parts = []
-    for (let i = 0; i < 4; i++) {
-        parts.push(Math.random().toString(36).slice(2))
+    const rn = new Uint32Array(4)
+    if (typeof 'crypto' !== 'undefined' && crypto.getRandomValues) {
+        crypto.getRandomValues(rn)
+    } else {
+        for (let i = 0; i < rn.length; i++) {
+            rn[i] = Math.floor(Math.random() * 4294967295)
+        }
     }
-    return parts.join('')
+    return Array.from(rn).map(n => n.toString(36)).join('')
 }
