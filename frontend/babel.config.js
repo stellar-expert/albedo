ed202006cd3cb6126f0316cbec7f38c311f7bfcc
orbitlens <orbit.lens@gmail.com>
2021-11-04 18:01:46 +0200

    Update dependencies

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -21,20 +21,11 @@ module.exports = function (api) {
             ]
         ],
         plugins: [
-            [
-                '@babel/plugin-proposal-class-properties',
-                {
-                    loose: true
-                }
-            ],
-            [
-                '@babel/plugin-proposal-private-methods',
-                {
-                    loose: true
-                }
-            ],
+            ['@babel/plugin-proposal-class-properties', {loose: true}],
+            ['@babel/plugin-proposal-private-methods', {loose: true}],
+            ['@babel/plugin-proposal-private-property-in-object', {loose: true}],
             '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-syntax-dynamic-import'
         ]
     }
 }
