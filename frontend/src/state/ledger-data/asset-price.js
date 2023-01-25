c02d85a13eed9dafb98b72aad54f4e0dd0e0ff3f
orbitlens <orbit.lens@gmail.com>
2023-01-25 22:50:12 +0200

    Fix asset price request issues

--- frontend/src/state/ledger-data/asset-price.js
+++ frontend/src/state/ledger-data/asset-price.js
@@ -1,4 +1,7 @@
 import {stringifyQuery} from '@stellar-expert/navigation'
+import {InMemoryClientCache} from '@stellar-expert/client-cache'
+
+const apiCache = new InMemoryClientCache({prefix: 'ac:'})
 
 /**
  * @param {'public'|'testnet'} network
@@ -6,11 +9,24 @@ import {stringifyQuery} from '@stellar-expert/navigation'
  * @return {Promise<{}>}
  */
 export async function fetchAssetPrices(network, asset) {
-    const ap = await fetch(`${explorerApiOrigin}/explorer/${network}/asset/price${stringifyQuery({asset})}`)
-        .then(r => r.json())
-    const res = {}
-    for (const {asset, price} of ap._embedded.records){
-        res[asset] = price
-    }
-    return res
+        const assets = {}
+        //map unique assets to claimable balances
+        for (let a of asset) {
+            assets[a] = 0
+        }
+        let uniqueAssets = Object.keys(assets)
+        //retrieve prices from the server
+        while (uniqueAssets.length > 0) {
+            //take 100 records for the batch
+            const batch = uniqueAssets.slice(0, 50)
+            uniqueAssets = uniqueAssets.slice(50)
+            //retrieve asset prices
+            const ap = await fetch(`${explorerApiOrigin}/explorer/${network}/asset/price${stringifyQuery({asset: batch})}`)
+                .then(r => r.json())
+            for (const {asset, price} of ap?._embedded?.records || []) {
+                assets[asset] = price
+            }
+        }
+
+        return assets
 }
