ebde209e245d3811566b01202de55750c219a012
Dmytro <dewaysd@gmail.com>
2023-05-03 18:29:21 +0300

    Hid the trustlines with empty balances

--- frontend/src/ui/wallet/balance/account-balance-view.js
+++ frontend/src/ui/wallet/balance/account-balance-view.js
@@ -3,6 +3,7 @@ import {parseAssetFromObject} from '@stellar-expert/asset-descriptor'
 import AccountAssetBalanceView from './account-asset-balance-view'
 import AccountPoolBalanceView from './account-pool-balance-view'
 import './account-balance.scss'
+import Bignumber from 'bignumber.js'
 
 function resolveBalanceComponent(balance, asset) {
     if (asset.poolId) return AccountPoolBalanceView
@@ -13,6 +14,7 @@ export default function AccountBalanceView({balance, account}) {
     const asset = parseAssetFromObject(balance)
     const component = resolveBalanceComponent(balance, asset)
     const balanceToRender = React.createElement(component, {asset, balance, account})
+    if (new Bignumber(balance.balance).toString() === new Bignumber(0).toString()) return <></>
     return <div className="account-balance-container">
         {balanceToRender}
         <hr className="flare"/>
